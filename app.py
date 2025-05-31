35c90d3f42f5543ea7fad9ce2241aa435fb2232e
jasmine <1556044893@qq.com>
2025-05-31 15:27:55 +0800

    客户端合并后房子bug显示修复

--- app.py
+++ app.py
@@ -38,22 +38,24 @@ def load_user(user_id):
 db.init_app(app)
 migrate = Migrate(app, db)  # 初始化 Flask-Migrate
 Bootstrap(app)
-# 将蓝图注册到app中
-app.register_blueprint(index_page, url_prefix='/')
-app.register_blueprint(list_page, url_prefix='/')
-app.register_blueprint(detail_page, url_prefix='/')
-app.register_blueprint(user_page, url_prefix='/')
-app.register_blueprint(chat_bp)
-cache.init_app(app)  # 初始化缓存
+            # 将蓝图注册到app中
+            # app.register_blueprint(index_page, url_prefix='/')
+            # app.register_blueprint(list_page, url_prefix='/')
+            # app.register_blueprint(detail_page, url_prefix='/')
+            # app.register_blueprint(user_page, url_prefix='/')
+            # app.register_blueprint(chat_bp)
+            # cache.init_app(app)  # 初始化缓存
 
 # 将蓝图注册到app中，为每个蓝图设置不同的 url_prefix
 app.register_blueprint(list_page, url_prefix='/list')
-app.register_blueprint(detail_page, url_prefix='/detail')
+app.register_blueprint(detail_page, url_prefix='/')#加上detail好像会报错呢
 app.register_blueprint(user_page, url_prefix='/user')
 app.register_blueprint(agent_page, url_prefix='/agent')
 app.register_blueprint(landlord_page, url_prefix='/landlord')
 app.register_blueprint(admin_page, url_prefix='/admin')
 app.register_blueprint(index_page)  # 首页蓝图不需要 url_prefix
+app.register_blueprint(chat_bp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
